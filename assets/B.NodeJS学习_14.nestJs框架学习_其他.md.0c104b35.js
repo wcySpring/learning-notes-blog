79e08f0b760c3b613c145fac0eec1a37252139ec
wcySpring <wcySpring@users.noreply.github.com>
2024-06-17 14:54:42 +0000

    deploy: 2de13309c2a39ac07ca7b4632f3fa0782db1393b

---
+++ assets/B.NodeJS学习_14.nestJs框架学习_其他.md.0c104b35.js
@@ -0,0 +1,13 @@
+import{_ as e,o as s,c as n,C as t}from"./chunks/framework.96db2af4.js";const h=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"B.NodeJS学习/14.nestJs框架学习/其他.md","filePath":"B.NodeJS学习/14.nestJs框架学习/其他.md","lastUpdated":1718635980000}'),c={name:"B.NodeJS学习/14.nestJs框架学习/其他.md"},o=t("pre",null,[t("code",null,`https://docs.nestjs.cn/10/firststeps
+
+
+	https://www.bilibili.com/video/BV1NW421c7kz/?p=2&spm_id_from=pageDriver&vd_source=1e4d2f8cd0a4f142adfbc4bf47b6c113
+
+
+	https://juejin.cn/post/7078847428455530526#heading-3
+
+	https://3rcd.com/wiki/nestjs-practise/chapter1
+
+
+	https://www.bilibili.com/video/BV1yt421w7vW?p=2&vd_source=1e4d2f8cd0a4f142adfbc4bf47b6c113
+`)],-1),a=[o];function d(i,r,p,_,f,l){return s(),n("div",null,a)}const b=e(c,[["render",d]]);export{h as __pageData,b as default};
